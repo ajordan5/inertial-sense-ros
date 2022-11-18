529a0398eaba6034279f3061275aa3bf01dbb748
lshums16 <landondshumway@gmail.com>
2022-11-18 12:15:14 -0700

    Got rid of redundant 'thing' call in inertial_sense_node.cpp

--- src/inertial_sense_node.cpp
+++ src/inertial_sense_node.cpp
@@ -3,7 +3,6 @@
 int main(int argc, char**argv)
 {
   rclcpp::init(argc, argv);
-  InertialSenseROS thing;
   auto node = std::make_shared<InertialSenseROS>();
   while (rclcpp::ok()) {
     rclcpp::spin_some(node);
